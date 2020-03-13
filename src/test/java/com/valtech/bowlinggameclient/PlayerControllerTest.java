ff985b318031546b9d16748fba70f28fe70279bc
amelhadjarbi <amel.arbi@valtech.com>
2020-03-13 17:33:56 +0100

    add file org.mockito.plugins.MockMaker unter  src/test/resources/mockito-extensions directory to allow Mocking the final class URI

--- src/test/java/com/valtech/bowlinggameclient/PlayerControllerTest.java
+++ src/test/java/com/valtech/bowlinggameclient/PlayerControllerTest.java
@@ -2,27 +2,43 @@ package com.valtech.bowlinggameclient;
 
 import java.net.URI;
 import com.valtech.bowlinggameclient.controller.PlayerController;
+import com.valtech.bowlinggameclient.dto.PlayerNameDto;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.http.HttpEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
+import static org.hamcrest.Matchers.any;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @RestClientTest(PlayerController.class)
 public class PlayerControllerTest {
+
     @Mock
     private RestTemplate restTemplate;
 
     @Mock
-    private URI playerURL;
+    private URI playerUrl;
 
     @InjectMocks
     private PlayerController playerController;
 
+    @Test
+    public void playerName() {
 
 
 
+
+
+    }
 
 }
