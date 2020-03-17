16ff8842a4aa814b8f65c37a4552478ff0210adb
amelhadjarbi <amel.arbi@valtech.com>
2020-03-17 10:38:42 +0100

    delete unnecessary import classes im PlayerControllerTest class

--- src/test/java/com/valtech/bowlinggameclient/PlayerControllerTest.java
+++ src/test/java/com/valtech/bowlinggameclient/PlayerControllerTest.java
@@ -2,46 +2,21 @@ package com.valtech.bowlinggameclient;
 
 import java.net.URI;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import com.valtech.bowlinggameclient.controller.PlayerController;
 import com.valtech.bowlinggameclient.dto.FrameDto;
 import com.valtech.bowlinggameclient.dto.GameDto;
 import com.valtech.bowlinggameclient.dto.PlayerNameDto;
 import com.valtech.bowlinggameclient.dto.WurfDto;
-import org.hamcrest.CoreMatchers;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatchers;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
-import static org.hamcrest.Matchers.any;
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.isNotNull;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import net.bytebuddy.agent.VirtualMachine;
 
 @RunWith(SpringRunner.class)
 @RestClientTest(PlayerController.class)
